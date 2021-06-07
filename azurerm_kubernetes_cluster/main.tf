0d04020ffd5c6fc438cb916d7aec9e9ff74249a2
Adrian Stobbe <adrian.stobbe@sap.com>
2021-06-07 13:14:57 +0200

    change to tf recommended config https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/service_principal_client_secret

--- azurerm_kubernetes_cluster/main.tf
+++ azurerm_kubernetes_cluster/main.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
-  version         = "~>1.5"
+  features {}
+
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
@@ -7,6 +8,12 @@ provider "azurerm" {
 }
 
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
   required_version = ">= 0.12"
   backend "local" {}
 }
